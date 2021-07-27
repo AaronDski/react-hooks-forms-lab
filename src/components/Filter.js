8a182cfda197f561cbe01174c32f12d37dc16e58
Aaron Dereszynski <aarondski123@gmail.com>
2021-07-26 20:02:12 -0600

    commit tree 4ad3f4bee0a8ca8406ebf56ae22a0f4b3546b004

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, onSearchChange, search }) {
+  function handleSearch(event) {
+    onSearchChange(event.target.value);
+    console.log(event.target.value)
+  }
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input 
+        value={search} 
+        onChange={handleSearch} 
+        type="text" 
+        name="search" 
+        placeholder="Search..."
+       />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
